0660641c8088643db22d602b4c7cec7f38d8fa75
edu <i497532>
2024-09-10 13:18:15 +0200

    Upload to DB and message queue

--- src/gateway/storage/util.py
+++ src/gateway/storage/util.py
@@ -1,5 +1,7 @@
 import pika, json
 
+import pika.spec
+
 def upload(f, fs, channel, access):
     try:
         fid = fs.put(f)
@@ -15,6 +17,15 @@ def upload(f, fs, channel, access):
     try:
         channel.basic_publish(
             exchange="",
+            routing_key="video",
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                # Persist messages in the queue in the event of a pod crash or restart (RabbitMQ pod)
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            )
         )
+    except:
+        fs.delete(fid)
+        return "internal server error", 500
     
     
